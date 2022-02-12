5a96210560a0dab9c9654ba55b486deedf39caec
Fiorlore <87369031+Fiorlore@users.noreply.github.com>
2022-02-13 00:47:11 +0100

    initial commit

---
+++ src/Dictionary.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
+
+export default function Dictionary(){
+
+    let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
+
+    function updateSearchTerm(event){
+        event.preventDefault();
+        setKeyword(event.target.value);
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleResponse(response) {
+        setResults(response.data[0]);
+       
+    }
+
+    return (
+    <div className="Dictionary">
+        <form onSubmit={handleSubmit}>
+            <input type="search" onChange={updateSearchTerm} />
+        </form>
+       <Results results={results} />
+    </div>
+    );
+}
